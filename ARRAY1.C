a20d5292379ec1b6d7166e59da8042b47ddf6002
Abhishek Kumar Gope <96175888+AbhiGope55@users.noreply.github.com>
2021-12-27 23:34:08 +0530

    Delete ARRAY1.C

--- ARRAY1.C
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
-  {
-    int N[5]={1,2,3,4,5};/*compile time initialization of arrary elements by joining the declaration and assignment statements*/
-    clrscr();
-    getch();
-  }
